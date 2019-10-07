58dca495b27df1b7b61f33cc96de7a9654f8d405
asenta2812 <chinghia28.1997@gmail.com>
2019-10-07 18:21:26 +0700

    login

--- booking-ticket/src/Pages/LoginPageAdmin/LoginAdmin.jsx
+++ booking-ticket/src/Pages/LoginPageAdmin/LoginAdmin.jsx
@@ -1,23 +1,4 @@
-import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles(theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        marginTop : 30
-    },
-    dense: {
-        marginTop: theme.spacing(),
-    },
-    menu: {
-        width: 200,
-    },
-}));
+import React, { Component } from 'react';   
 
 export default class LoginAdmin extends Component {
     render() {
@@ -26,25 +7,9 @@ export default class LoginAdmin extends Component {
             <div className="login-admin">
                 <div className="login-card">
                     <h3 className="title__h3 text-center">Đăng nhập</h3>
-                    <TextField
-                        id="taiKhoan"
-                        label="Tài khoản"
-                        className={useStyles.textField}
-                        placeholder="Tài khoản..."
-                        // onChange={handleChange('name')}
-                        fullWidth
-                        margin="normal"
-                    />
-                    <TextField
-                        id="matKhau"
-                        label="Mật khẩu"
-                        className={useStyles.textField}
-                        placeholder="Mật khẩu..."
-                        // onChange={handleChange('name')
-                        margin="normal"
-                        fullWidth
-                    />
-                
+                    <form>
+                       
+                    </form>
                 </div>
             </div>
         )
