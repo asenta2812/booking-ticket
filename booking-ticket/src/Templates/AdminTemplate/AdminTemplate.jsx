dedf716f9cf26ea8d00e8e22ba6961975046d123
asenta2812 <chinghia28.1997@gmail.com>
2019-10-20 18:17:41 +0700

    up

--- booking-ticket/src/Templates/AdminTemplate/AdminTemplate.jsx
+++ booking-ticket/src/Templates/AdminTemplate/AdminTemplate.jsx
@@ -2,9 +2,12 @@ import React, { Fragment } from "react"
 import HeaderAdmin from "../../component/layouts/Header/HeaderAdmin/HeaderAdmin"
 import FooterAdmin from "../../component/layouts/Footer/FooterAdmin/FooterAdmin"
 import { Route } from 'react-router-dom'
+import SideBarAdmin from "../../component/layouts/SideBarAdmin/SideBarAdmin"
+// import './sb-admin-2.min.css';
 const AdminLayout = (props) => {
     return <Fragment>
         <HeaderAdmin />
+        <SideBarAdmin/>
         {props.children}
         <FooterAdmin />
     </Fragment>
